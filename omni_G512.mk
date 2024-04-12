c572b27d1a54a9e04088af76881dea09e531673f
Efe Timur Turan <tasalxntn@gmail.com>
2024-04-12 20:57:43 +0300

    Update omni_G512.mk

--- omni_G512.mk
+++ omni_G512.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from G512 device
 $(call inherit-product, device/general/G512/device.mk)
